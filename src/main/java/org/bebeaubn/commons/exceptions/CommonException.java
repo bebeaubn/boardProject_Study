6102945e1d16f7663f87b4fdabdc731132f5b30f
bebeaubn <yebin0213@naver.com>
2023-11-10 16:44:38 +0900

    설정중

--- src/main/java/org/bebeaubn/commons/exceptions/CommonException.java
+++ src/main/java/org/bebeaubn/commons/exceptions/CommonException.java
@@ -1,23 +1,20 @@
 package org.bebeaubn.commons.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 public class CommonException extends RuntimeException{
     private HttpStatus status;
 
-    public CommonException(String message){
-        this(message, HttpStatus.INTERNAL_SERVER_ERROR);  //500
+    public CommonException(String message) {
+        this(message, HttpStatus.INTERNAL_SERVER_ERROR); // 500
     }
 
-    public CommonException(String message, HttpSecurity status){
+    public CommonException(String message, HttpStatus status) {
         super(message);
         this.status = status;
-
     }
 
-    public HttpStatus getStatus(){
+    public HttpStatus getStatus() {
         return status;
     }
-
 }

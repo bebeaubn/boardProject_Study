2a1f44e81891aba76618763f1345ddeb357e9907
bebeaubn <yebin0213@naver.com>
2023-11-13 18:18:30 +0900

    pc 모바일 나누는중

--- src/main/java/org/bebeaubn/commons/interceptors/commonInterceptor.java
+++ src/main/java/org/bebeaubn/commons/interceptors/commonInterceptor.java
@@ -3,6 +3,7 @@ package org.bebeaubn.commons.interceptors;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.bebeaubn.commons.Utils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -15,16 +16,29 @@ public class commonInterceptor implements HandlerInterceptor {
         return true;
     }
 
-    private void init(HttpServletRequest request){
+    private void init(HttpServletRequest request) {
         HttpSession session = request.getSession();
 
         /* PC, mobile 수동 처리 변경 S */
         String device = request.getParameter("device");
         System.out.println("device:" + device);
         if (device != null && !device.isBlank()) {
-            session.setAttribute("device",device.toLowerCase().equals("mobile")?"mobile" : "pc");
+            session.setAttribute("device", device.toLowerCase().equals("mobile") ? "mobile" : "pc");
         }
 
         /* PC, MOBILE 수동처리 변경 E */
+
+        /* 로그인 페이지 아닐경우 로그인 유효성 검사 세션 삭제 처리 */
+
+        String URI = request.getRequestURI();
+        if(URI.indexOf("/member/login") == -1) {
+            Utils.loginInit(session);
+
+        }
+
+
+
+
     }
+
 }

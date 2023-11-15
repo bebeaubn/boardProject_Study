053bcf4eb0bce06134dc6e17cf82892e92351693
bebeaubn <yebin0213@naver.com>
2023-11-15 16:31:56 +0900

    으쌰라 으쌰

--- src/main/java/org/bebeaubn/entities/Member.java
+++ src/main/java/org/bebeaubn/entities/Member.java
@@ -1,12 +1,15 @@
 package org.bebeaubn.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 import org.bebeaubn.commons.constants.MemberType;
-import org.springframework.data.annotation.Id;
+
 
 import java.time.LocalDateTime;
 
 @Data
+@Entity
 public class Member {
 
     @Id
--- src/main/java/org/bebeaubn/repositories/MemberRepository.java
+++ src/main/java/org/bebeaubn/repositories/MemberRepository.java
@@ -1,11 +1,11 @@
 package org.bebeaubn.repositories;
 
 import org.bebeaubn.entities.Member;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
 

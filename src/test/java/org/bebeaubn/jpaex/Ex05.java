023680191bfa22a06a2b466bb6acca586cf7f555
bebeaubn <yebin0213@naver.com>
2023-11-16 18:19:59 +0900

    설정 수정

--- src/test/java/org/bebeaubn/jpaex/Ex05.java
+++ src/test/java/org/bebeaubn/jpaex/Ex05.java
@@ -1,4 +1,31 @@
 package org.bebeaubn.jpaex;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.bebeaubn.entities.BoardData;
+import org.bebeaubn.entities.QBoardData;
+import org.bebeaubn.repositories.BoardDataRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+@SpringBootTest
+@TestPropertySource(properties = "spring.profiles.active=test")
 public class Ex05 {
+    @Autowired
+    private BoardDataRepository repository;
+
+    @Test
+    void test1(){
+        QBoardData boardData =QBoardData.boardData;
+       // BooleanExpression cond1 =boardData.subject.contains("목");
+
+        List<BoardData> items = (List<BoardData>) repository.findAll(boardData.subject.contains("목"));
+        items.stream().forEach(System.out::println);
+
+
+    }
+
 }
